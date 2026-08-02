1a7e8eb6d23d8be70383438aff609dd771636b1a
Velocity <leathergate@proton.me>
2026-08-02 23:36:50 +0800

    feat: add find_true_elo_page for feat_FindPageOfEloUsingBinarySearch

--- feat_FindPageOfEloUsingBinarySearchv2.py
+++ feat_FindPageOfEloUsingBinarySearchv2.py
@@ -5,9 +5,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 ### config ###
-DEBUG = True
+DEBUG = False
 VERBOSE = True
+
 ### --- ###
+
+### variable declaration ###
+low_elo = 2000
+high_elo = 2100
+### --- ###
+
+def find_true_elo_page(general_page: int, is_low_elo: bool, is_high_elo: bool) -> int:
+  if is_low_elo == True:
+    elo_to_find = low_elo - 1
+  if is_high_elo == True:
+    elo_to_find = high_elo + 1
+
+  pagination = general_page
+  while True:
+    list_of_elo_in_page = open_leaderboard_page_retrieve_elo(pagination)
+    if str(elo_to_find) in list_of_elo_in_page:
+      return pagination
+    elif is_low_elo == True:
+      pagination += 1
+    elif is_high_elo == True:
+      pagination -= 1
+
+
 
 def open_leaderboard_page_retrieve_elo(page: int) -> list:           # returns elo list
   ### variable declaration ###
@@ -37,6 +61,7 @@ def open_leaderboard_page_retrieve_elo(page: int) -> list:           # returns e
 
   contains_elo_array = [x.text for x in elements]
 
+  
   multiple_elo_list = []
   for index in eloindexes:          # buggy
     try: 
@@ -45,6 +70,9 @@ def open_leaderboard_page_retrieve_elo(page: int) -> list:           # returns e
       break
 
   unique_elo_list = list(dict.fromkeys(multiple_elo_list))
+
+  if DEBUG == True or VERBOSE == True:
+    print(f'(*) Found elo on the page: {unique_elo_list}')            
 
   driver.close()
   return unique_elo_list
@@ -59,9 +87,8 @@ def ELO_PAGE_binary_search(TARGET_elo_given: int, low_elo_range: bool, high_elo_
   while True:
     midpoint = left + (right - left) // 2
     current_item = open_leaderboard_page_retrieve_elo(midpoint)
-    print(current_item)
      
-    if len(current_item) == 1 or high_elo_range == True:
+    if len(current_item) == 1 or high_elo_range == True:      # found a bug: (*) Found elo on the page: ['2101', '2100', '2099']
       current_item = int(current_item[0])
     elif low_elo_range == True:
       current_item = int(current_item[-1])
@@ -73,11 +100,36 @@ def ELO_PAGE_binary_search(TARGET_elo_given: int, low_elo_range: bool, high_elo_
       right = midpoint - 1
     else:
       left = midpoint + 1
-         
+
+    ### reexamine for the bug ###
+    bug_fix = 'bug_fix'
 
 if __name__ == '__main__':
-  # result = ELO_PAGE_binary_search(1400, True, False)
-  # result = ELO_PAGE_binary_search(1500, False, True)
-  #result = ELO_PAGE_binary_search(1399, True, False)  # between 1399 - 1400 for minimum
-  result = ELO_PAGE_binary_search(1501, False, True) # between 1500 - 1501 
-  print(result)
+  ### low elo ###
+  if VERBOSE == True:
+    print(f'[*] Searching for the General Chess.com Page of {low_elo}')
+  low_elo_general_page = ELO_PAGE_binary_search(low_elo, True, False)
+  if DEBUG == True or VERBOSE == True:
+    print(f'[*] Found the General Chess.com Page for {low_elo}: {low_elo_general_page}')
+
+  ### find true page low elo ###      # there will be 2 cases, ex. 1400 and 1399 co-exist in the page; 1400 and 1399 are on the separate page
+  if VERBOSE == True:
+    print(f'[*] Searching for the Real Chess.com Page of {low_elo}')         
+  low_elo_true_page = find_true_elo_page(low_elo_general_page, True, False)
+
+  ### high elo ###
+  if VERBOSE == True:
+      print(f'[*] Searching for the General Chess.com Page of {high_elo}')
+  high_elo_general_page = ELO_PAGE_binary_search(high_elo, False, True)
+  if DEBUG == True or VERBOSE == True:
+    print(f'[*] Found the General Chess.com Page for {high_elo}: {high_elo_general_page}')
+
+  ### find true page high elo ###
+  if VERBOSE == True:
+      print(f'[*] Searching for the Real Chess.com Page of {high_elo}')    
+  high_elo_true_page = find_true_elo_page(high_elo_general_page, False, True)
+
+  if VERBOSE == True:
+    print(f'[*] true page of low elo: {low_elo_true_page}')
+    print(f'[*] true page of high elo: {high_elo_true_page}')
+  
